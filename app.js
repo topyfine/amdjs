7efdb99a3323e663df925e49766aabd0124f11d7
杨帆 <yangfan@shuangke.net.cn>
2018-11-23 18:07:34 +0800

    模块amd

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 console.log(">>> Hello requirejs!");
-require(["lib/jquery"], function ($) {
+require.config({
+    baseUrl: '/amdjs/lib',
+    paths: {
+        'jquery': 'jquery3',
+        "foo": "non.amd"
+    },
+    shim: {
+        "foo": {
+            deps: ["jquery"],
+            exports: "Foo"
+        }
+    }
+});
+require(["foo", "bar"], function (foo, bar) {
     console.log($);
+    // console.log(foo.name);
+    // 定义的foo模块是一个对象
+    foo.depts();
+    // 定义的bar模块是1个函数
+    bar();
 });
